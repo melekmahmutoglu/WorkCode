0f9669d0af250103f88a0bdc2a0613b9e7bcf71a
Melek Mahmutoğlu <melekmahmutoglu.555@gmail.com>
2023-10-04 21:20:36 +0300

    last added

--- Pages/Submit/Submit.js
+++ Pages/Submit/Submit.js
@@ -2,15 +2,16 @@ import { View, Text, SafeAreaView, FlatList } from 'react-native'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import JobComponent from '../../Component/JobComponent/JobComponent'
+import styles from './Submit.style'
 
 const Submit = () => {
     const submitList = useSelector((state) => state.submit.submitList)
     return (
-        <SafeAreaView>
+        <SafeAreaView style={styles.container}>
             {
                 submitList.length === 0 ?
-                    <View>
-                        <Text>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>
                             You haven't added any job to submited yet!
                         </Text>
                     </View> :
--- Pages/Submit/Submit.style.js
+++ Pages/Submit/Submit.style.js
@@ -2,4 +2,17 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
 
+    titleContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: '#ff6666'
+    },
+    container: {
+        flex: 1,
+    }
 })
